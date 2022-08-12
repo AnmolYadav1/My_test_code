d9ff28339f8b1464b14a2eae0dae5ce0c7301aa1
AnmolYad <anmolyadav5318@gmail.com>
2022-08-12 21:53:29 +0530

    third commit

---
+++ src/BRM_Lab.java
@@ -0,0 +1,8 @@
+
+public class BRM_Lab {
+	public static void main(String[] args) {
+		   System.out.println("Hello world");
+
+}
+}
+
--- src/Brm_New_Lab.java
+++ src/Brm_New_Lab.java
@@ -2,7 +2,9 @@
 public class Brm_New_Lab {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 	   System.out.println(" hello class");
+	   
 
 	}
 
